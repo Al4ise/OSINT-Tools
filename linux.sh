09a06a0197e6f1bf40e32d03d8839ee8613886e6
Martin Pelteshki <39273158+Al4ise@users.noreply.github.com>
2023-11-19 16:22:27 +0200

    work

--- linux.sh
+++ linux.sh
@@ -1,11 +1,34 @@
 #!/usr/bin/bash
 SOURCE="$(dirname "$(realpath "$0")")"
 
+# menu
+echo "What Virtualization Software are you on?:"
+echo "[1] Virtualbox"
+echo "[2] UTM/QEMU"
+echo "[3] None/Other"
+read -rp "Choose an option: " option
+case $option in
+   1) echo "Installing Virtualbox-specific software..."
+       sudo adduser osint vboxsf
+       sudo rcvboxadd setup
+       sudo apt install -y virtualbox-guest-utils
+   ;;
+   2) echo "Installing UTM/QEMU-specific software..."
+       sudo apt install -y qemu-guest-agent
+       sudo systemctl enable qemu-guest-agent
+       sudo systemctl start qemu-guest-agent
+   ;;
+   3) echo "No virtualization-specific software will be installed."
+   ;;
+   *) echo "Invalid option. Try another one." && exit
+   ;;
+esac
+
 pythonInstall() {
     local name="$1"
 
     mkdir -p ~/Downloads/Programs/"$name"
-    cd ~/Downloads/Programs/"$name"
+    cd ~/Downloads/Programs/"$name" || exit
     python3 -m venv "$name"Environment
     source "$name"Environment/bin/activate
     sudo pip install -U "$name" 2>/dev/null
@@ -15,74 +38,103 @@ pythonInstall() {
 pyGitInstall() {
     local url="$1"
     local name="$(basename "$1" .git)"
-    
-    cd ~/Downloads/Programs
-    git clone "$url"
-    cd "$name"
-    python3 -m venv "$name"Environment
+    local req_path="$2"
+    if [ -z "$req_path" ]; then
+        req_path="$name/requirements.txt"
+    else
+        req_path="$name/$req_path"
+    fi
+
+    cd ~/Downloads/Programs || exit
+    if [ -d "$name" ]; then
+        cd "$name" || exit
+        git pull
+    else
+        git clone --recursive "$url"
+        cd "$(dirname "$req_path")" || exit
+        python3 -m venv "$name"Environment
+    fi
     source "$name"Environment/bin/activate
-    sudo pip install -r requirements.txt 2>/dev/null
+    sudo pip install -r "$(basename "$req_path")" 2>/dev/null
     deactivate
 }
 
-sudo apt update
-sudo apt install curl
-sudo apt purge -y apport apport-symptoms popularity-contest ubuntu-report whoopsie
-sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-### google earth keyring
+# keyrings/repos
+wget -q -O /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
 sudo add-apt-repository -y ppa:mozillateam/ppa
-sudo apt update
-sudo apt install -y build-essential unrar brave-browser dkms gcc make perl libncurses5-dev gnupg2 tor python3-pip dh-python python3-all python3-stdeb python3-pyqt5 python3-gpg python3-requests python3-socks python3-packaging ffmpeg vlc libffi-dev jq ripgrep bleachbit kazam libcanberra-gtk-module httrack webhttrack subversion mat2 libimage-exiftool-perl mediainfo-gui default-jre git python3-venv
-
 echo '
 Package: *
 Pin: release o=LP-PPA-mozillateam
 Pin-Priority: 1001
-' | sudo tee /etc/apt/preferences.d/mozilla-firefox
-echo 'Unattended-Upgrade::Allowed-Origins:: "LP-PPA-mozillateam:${distro_codename}";' | sudo tee /etc/apt/apt.conf.d/51unattended-upgrades-firefox
+' | sudo tee /etc/apt/preferences.d/mozilla-firefox > /dev/null
+echo 'Unattended-Upgrade::Allowed-Origins:: "LP-PPA-mozillateam:${distro_codename}";' | sudo tee /etc/apt/apt.conf.d/51unattended-upgrades-firefox > /dev/null
 
-sudo apt install -y firefox --allow-downgrades
+sudo apt update
+sudo apt install -y curl build-essential unrar brave-browser dkms gcc make perl libncurses5-dev gnupg2 tor python3-pip dh-python python3-all python3-stdeb python3-pyqt5 python3-gpg python3-requests python3-socks python3-packaging ffmpeg vlc libffi-dev jq ripgrep bleachbit kazam libcanberra-gtk-module httrack webhttrack subversion mat2 libimage-exiftool-perl mediainfo-gui default-jre git python3-venv
+
+# purges
+sudo apt purge -y apport apport-symptoms popularity-contest ubuntu-report whoopsie
+sudo snap remove --purge firefox
 
 sudo apt install -y qemu-guest-agent
 sudo systemctl enable qemu-guest-agent
 sudo systemctl start qemu-guest-agent
 
-# google earth
-wget http://dl.google.com/dl/earth/client/current/google-earth-stable_current_amd64.deb
-sudo rm google-earth-stable_current_amd64.deb
-sudo apt install -y ./google-earth-stable_current_amd64.deb
-
 sudo apt update --fix-missing
 sudo apt -y upgrade
 sudo apt --fix-broken install -y
 
-sudo snap remove --purge firefox
-sudo snap install chromium
-sudo snap install amass
-
+#firefox
+sudo apt install -y firefox --allow-downgrades
 firefox &
 sleep 30
 pkill -f firefox
-curl -O https://inteltechniques.com/data/osintbook10/ff-template.zip
 
+# firefox profile
+wget https://inteltechniques.com/data/osintbook10/ff-template.zip
 unzip ff-template.zip -d ~/.mozilla/firefox/
-cd ~/.mozilla/firefox/ff-template/
-cp -R * ~/.mozilla/firefox/*.default-release
-rm ff-template.zip
+mv ~/.mozilla/firefox/ff-template/* ~/.mozilla/firefox/*.default-release/
+rm -f ff-template.zip
+rm -rf ~/.mozilla/firefox/ff-template
 
+# snap
+sudo snap install chromium
+sudo snap install amass
+
+# google earth
+cd ~/Desktop || exit
+wget http://dl.google.com/dl/earth/client/current/google-earth-stable_current_amd64.deb
+sudo rm google-earth-stable_current_amd64.deb
+sudo apt install -y ./google-earth-stable_current_amd64.deb
+
+# pip installs
 sudo pip install -U youtube-dl
 sudo pip install -U yt-dlp
+sudo pip install -U testresources
+sudo pip install -U webscreenshot
+sudo pip install -U pipx
+sudo pip install -U nested-lookup
 
-sudo pip install testresources 2>/dev/null
-sudo pip install webscreenshot 2>/dev/null
-sudo pip install pipx 2>/dev/null
-sudo pip install nested-lookup 2>/dev/null
+# pipx s
+pipx ensurepath
+pipx install ghunt
 
-ln -sf $SOURCE/vm-files/scripts ~/Documents/scripts
-ln -sf $SOURCE/vm-files/icons ~/Documents/icons
-sudo cp $SOURCE/vm-files/shortcuts/* /usr/share/applications/
-ln -sf $SOURCE/tools ~/Desktop/Tools
+# whisper
+sudo pip install -U openai-whisper
+mkdir -p ~/.cache/whisper
+cd ~/.cache/whisper || exit
+wget https://openaipublic.azureedge.net/main/whisper/models/9ecf779972d90ba49c06d968637d720dd632c55bbf19d441fb42bf17a411e794/small.pt
+
+# links
+ln -sf "$SOURCE"/vm-files/scripts ~/Documents/scripts
+ln -sf "$SOURCE"/vm-files/icons ~/Documents/icons
+ln -sf "$SOURCE"/tools ~/Desktop/Tools
+
+for i in "$SOURCE"/vm-files/shortcuts/*; do
+    sudo ln -sf "$SOURCE"/vm-files/shortcuts/"$i" /usr/share/applications/"$i"
+done
 
 pythonInstall "streamlink"
 pythonInstall "instalooter"
@@ -113,76 +165,60 @@ pyGitInstall "https://github.com/opsdisk/metagoofil.git"
 pyGitInstall "https://github.com/MalloyDelacroix/DownloaderForReddit.git"
 pyGitInstall "https://github.com/smicallef/spiderfoot.git"
 #pyGitInstall "https://github.com/Datalux/Osintgram.git"
+pyGitInstall "https://github.com/GuidoBartoli/sherloq.git" "gui/requirements.txt"
+pyGitInstall "https://github.com/lanmaster53/recon-ng.git" "REQUIREMENTS"
 
 #snoop
 mkdir -p ~/Downloads/Programs/snoop
-cd ~/Downloads/Programs/snoop
+cd ~/Downloads/Programs/snoop || exit
 wget https://github.com/snooppr/snoop/releases/latest/download/Snoop.for.GNU_Linux.rar
 unrar x Snoop.for.GNU_Linux.rar
 chmod +x snoop_cli
 rm Snoop.for.GNU_Linux.rar
+ln -sf ~/Downloads/Programs/snoop/snoop_cli /usr/local/bin/snoop
 
 #h8mail
 pythonInstall "h8mail"
-
-cd ~/Downloads
+cd ~/Downloads || exit
 h8mail -g
 sed -i 's/\;leak\-lookup\_pub/leak\-lookup\_pub/g' h8mail_config.ini
 
 #archivebox
 pythonInstall "archivebox"
 mkdir -p ~/Documents/archivebox
-cd ~/Documents/archivebox
+cd ~/Documents/archivebox || exit
 archivebox init
 
-cd ~/Downloads
+# ripme.jar
+cd ~/Downloads || exit
 wget https://github.com/ripmeapp/ripme/releases/latest/download/ripme.jar
 chmod +x ripme.jar
 
-pipx ensurepath
-pipx install ghunt
-pipx ensurepath
-
-cd ~/Downloads/Programs
-git clone https://github.com/FortyNorthSecurity/EyeWitness.git
-cd EyeWitness/Python/setup
+# eyewitness
+cd ~/Downloads/Programs || exit
+if [ -d "EyeWitness" ]; then
+    cd EyeWitness || exit
+    git pull
+else
+    git clone --recursive https://github.com/FortyNorthSecurity/EyeWitness.git
+fi
+cd EyeWitness/Python/setup || exit
 sudo ./setup.sh
-cd ~/Downloads/Programs
-wget https://github.com/mozilla/geckodriver/releases/download/v0.32.0/geckodriver-v0.32.0-linux-aarch64.tar.gz
-tar -xvzf geckodriver*
-chmod +x geckodriver
-sudo mv geckodriver /usr/local/bin
 
-cd ~/Downloads/Programs
-git clone https://github.com/GuidoBartoli/sherloq.git
-cd sherloq/gui
-python3 -m venv sherloqEnvironment
-source sherloqEnvironment/bin/activate
-sudo pip install -r requirements.txt 2>/dev/null
-deactivate
-
-cd ~/Downloads/Programs
-git clone https://github.com/lanmaster53/recon-ng.git
-cd recon-ng
-python3 -m venv recon-ngEnvironment
-source recon-ngEnvironment/bin/activate
-sudo pip install -r REQUIREMENTS 2>/dev/null
-deactivate
-
-sudo pip install -U openai-whisper
-mkdir -p ~/.cache/whisper
-cd ~/.cache/whisper
-wget https://openaipublic.azureedge.net/main/whisper/models/9ecf779972d90ba49c06d968637d720dd632c55bbf19d441fb42bf17a411e794/small.pt
-cd ~/Documents/scripts
-
-cd ~/Downloads/Programs
-git clone https://github.com/AmIJesse/Elasticsearch-Crawler.git
+# Elasticsearch-Crawler
+cd ~/Downloads/Programs || exit
+if [ -d "Elasticsearch-Crawler" ]; then
+    cd Elasticsearch-Crawler || exit
+    git pull
+else
+    git clone --recursive https://github.com/AmIJesse/Elasticsearch-Crawler.git
+fi
 
 #gsettings set org.gnome.desktop.background picture-uri ''
 gsettings set org.gnome.desktop.background primary-color 'rgb(66, 81, 100)'
 gsettings set org.gnome.shell favorite-apps []
 gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
-gsettings set org.gnome.shell favorite-apps "['firefox.desktop', 'chromium_chromium.desktop', 'torbrowser.desktop', 'org.gnome.Nautilus.desktop', 'org.gnome.Terminal.desktop', 'updates.desktop', 'tools.desktop', 'video-download.desktop', 'video-utilities.desktop', 'video-stream.desktop', 'instagram.desktop', 'galleries.desktop', 'users-emails.desktop', 'captures.desktop', 'domains.desktop', 'metadata.desktop', 'archives.desktop', 'documents.desktop', 'breaches-leaks.desktop', 'reddit.desktop', 'spiderfoot.desktop', 'recon-ng.desktop', 'api.desktop', 'google-earth-pro.desktop', 'kazam.desktop', 'gnome-control-center.desktop']"
+gsettings set org.gnome.shell favorite-apps "['firefox.desktop', 'brave-browser.desktop', 'chromium_chromium.desktop', 'torbrowser.desktop', 'org.gnome.Nautilus.desktop', 'org.gnome.Terminal.desktop', 'updates.desktop', 'tools.desktop', 'video-download.desktop', 'video-utilities.desktop', 'video-stream.desktop', 'instagram.desktop', 'galleries.desktop', 'users-emails.desktop', 'captures.desktop', 'domains.desktop', 'metadata.desktop', 'archives.desktop', 'documents.desktop', 'breaches-leaks.desktop', 'reddit.desktop', 'spiderfoot.desktop', 'recon-ng.desktop', 'api.desktop', 'google-earth-pro.desktop', 'kazam.desktop', 'gnome-control-center.desktop']"
 gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 32
 echo
 read -rsp $'Press enter to continue, reboot, then launch the Updates script.\n'
--- vm-files/scripts/spiderfoot.sh
+++ vm-files/scripts/spiderfoot.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-cd ~/Downloads/Programs/spiderfoot
+cd ~/Downloads/Programs/spiderfoot || exit
 python3 ./sf.py -l 127.0.0.1:5001 & 
 sleep 5
 firefox  http://127.0.0.1:5001
